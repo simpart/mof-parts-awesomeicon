98d50dc7b2251c7019bd378b4e177c7595b10cd7
simpart <simpart>
2019-08-31 15:48:06 +0900

    add link

--- index.js
+++ index.js
@@ -1,51 +1,57 @@
 /**
  * @file mofron-comp-aweicon/index.js
- * @brief [fontawesome](https://fontawesome.com/) icon component
- * @attention it needs link tag that has a path to font-awesome
+ * @brief awesome icon component
  * @author simpart
  */
 const mf = require('mofron');
 const Text = require('mofron-comp-text');
+const Link = require('mofron-event-link');
 
 mf.comp.AweIcon = class extends Text {
     /**
      * initialize component
      * 
-     * @param (string) same as mofron-comp-text
+     * @param (mixed) icon parameter
+     *                object: component option
+     * @param (prm) path parameter
+     * @pmap icon,path
      * @type private
      */
     constructor (po) {
         try {
             super();
-            this.name("AweIcon");
+            this.name('AweIcon');
+            this.prmMap('icon', 'path');
             this.prmOpt(po);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
-    
+
     /**
      * initialize dom contents
-     *
+     * 
      * @type private
      */
     initDomConts() {
         try {
-            super.initDomConts('i');
-            this.size("0.8rem");
-            this.mainColor([220,220,220]);
-        } catch (e) {
+	    super.initDomConts();
+	    this.style(
+	        { 'font-family' : null },
+		{ locked :true }
+	    );
+	} catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
     /**
-     * same as icon
-     *
-     * @param (string) icon simbol, class value [same as 'icon' parameter]
-     * @return (string) icon simbol, class value
+     * icon config
+     * 
+     * @param (string) icon config
+     * @return (string) icon config
      * @type parameter
      */
     text (prm) {
@@ -56,17 +62,17 @@ mf.comp.AweIcon = class extends Text {
     }
     
     /**
-     * icon value
-     *
-     * @param (string) icon simbol, class value
-     * @return (string) icon simbol, class value
+     * awesome icon config
+     * 
+     * @param (string) icon config
+     * @return (string) icon config
      * @type parameter
      */
     icon (prm) {
         try {
            if (undefined === prm) {
                /* getter */
-               return this.member('icon', 'string');
+               return this.member('icon');
            }
            /* setter */
            this.target().className(prm, (null !== this.icon()) ? true : false);
@@ -78,10 +84,10 @@ mf.comp.AweIcon = class extends Text {
     }
     
     /**
-     * add link tag
-     *
-     * @param (string) css path for font-awesome
-     * @return (string) css path for font-awesome
+     * icon path
+     * 
+     * @param (string) icon path
+     * @return (string) icon path
      * @type parameter
      */
     path (prm) {
@@ -96,6 +102,34 @@ mf.comp.AweIcon = class extends Text {
             }
             return this.member('path', 'string', prm);
         } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * link path
+     * 
+     * @param (string) link path 
+     * @param (boolean) true: open link with new tab
+     *                  false: jump to link
+     * @return (array) [link path, tab flag]
+     * @type parameter
+     */
+    link (prm, tab) {
+        try {
+	    let link = this.event(["Link", "FontAwesome"]);
+            if (undefined === prm) {
+                /* getter */
+		return (null === link) ? null : link.url();
+	    }
+	    /* setter */
+	    if (null === link) {
+	        link = new Link({ tag: "FontAwesome" });
+                this.event(link);
+	    }
+	    link.url(prm, tab);
+	} catch (e) {
             console.error(e.stack);
             throw e;
         }
